fddb1c43fe5fe673b16482b8bac255c94d4f2975
Clemens Schmid <clemens@nevrome.de>
2020-05-09 14:43:01 +0200

    cleaning ascii world map code

--- supp_prep/prepare_world_map_data.R
+++ supp_prep/prepare_world_map_data.R
@@ -1,13 +1,24 @@
-hu <- rnaturalearthdata::countries110 %>% sf::st_as_sf()
+plot_width <- 120
+plot_height <- 30
 
-pu <- fasterize::fasterize(hu, raster::raster(xmn = -180, xmx = 180, ymn = -90, ymx = 90, nrow = 20, ncol = 80)) 
+points_of_interest <- tibble::tibble(
+  x = c(9, 30, -70),
+  y = c(45, 45, -50)
+)
 
-schu <- t(raster::as.matrix(pu))
+world_map_raster <- raster::rasterize(
+  rnaturalearthdata::countries110, 
+  raster::raster(xmn = -180, xmx = 180, ymn = -90, ymx = 90, nrow = plot_height, ncol = plot_width)
+)
 
-raster::xyFromCell(pu, cell = which(!is.na(schu))) %>% 
-  tibble::as_tibble() -> gnu
+world_map_matrix <- t(raster::as.matrix(world_map_raster))
 
-plot_width <- 80
+landmass_cells <- raster::xyFromCell(
+  world_map_raster, 
+  cell = which(!is.na(world_map_matrix))
+) %>% 
+  tibble::as_tibble()
+
 x_breaks <- tibble::tibble(
   x_plot_pos = 1:plot_width
 ) %>%
@@ -16,7 +27,6 @@ x_breaks <- tibble::tibble(
     x_coord_stop = -180 + 360/plot_width * x_plot_pos
   )
 
-plot_height <- 20
 y_breaks <- tibble::tibble(
   y_plot_pos = 1:plot_height
 ) %>%
@@ -25,47 +35,43 @@ y_breaks <- tibble::tibble(
     y_coord_stop = -90 + 180/plot_height * y_plot_pos
   )
 
-ju <- gnu %>%
+landmass_cells_plot <- landmass_cells %>%
   dplyr::mutate(
     x = as.integer(cut(x, breaks = c(x_breaks$x_coord_start[1], x_breaks$x_coord_stop), labels = 1:plot_width)),
     y = as.integer(cut(y, breaks =  c(y_breaks$y_coord_start[1], y_breaks$y_coord_stop), labels = 1:plot_height)),
-    land = 1
+    land = TRUE
   )
 
-points_of_interest <- tibble::tibble(
-  x = c(9, 30),
-  y = c(45, 45)
-) %>%
+points_of_interest_cells_plot <- points_of_interest %>%
   dplyr::mutate(
     x = as.integer(cut(x, breaks = c(x_breaks$x_coord_start[1], x_breaks$x_coord_stop), labels = 1:plot_width)),
     y = as.integer(cut(y, breaks =  c(y_breaks$y_coord_start[1], y_breaks$y_coord_stop), labels = 1:plot_height)),
-    position = 2
+    position = TRUE
   )
 
-gu <- expand.grid(
+plot_grid <- expand.grid(
   x = 1:plot_width,
   y = 1:plot_height
 ) %>% 
   dplyr::left_join(
-    ju, by = c("x", "y")
+    landmass_cells_plot, by = c("x", "y")
   ) %>%
   dplyr::left_join(
-    points_of_interest, by = c("x", "y")
+    points_of_interest_cells_plot, by = c("x", "y")
   ) %>%
   dplyr::mutate(
-    land =  tidyr::replace_na(land, 0),
-    position =  tidyr::replace_na(position, 0),
-    z = land + position
+    land =  tidyr::replace_na(land, FALSE),
+    position =  tidyr::replace_na(position, FALSE)
   )
 
-for (ro in seq(20, 1, -1)) {
-  for (co in 1:80) {
-    cur <- gu$z[gu$x == co & gu$y == ro]
-    if (cur == 0) {
+for (ro in seq(plot_height, 1, -1)) {
+  for (co in 1:plot_width) {
+    cur <- plot_grid[plot_grid$x == co & plot_grid$y == ro,]
+    if (!cur$land) {
       cat(" ")
-    } else if (cur == 1) {
+    } else if (cur$land & !cur$position) {
       cat(".")
-    } else if (cur > 1) {
+    } else if (cur$position) {
       cat("X")
     }
   }
