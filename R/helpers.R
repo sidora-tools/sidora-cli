3c3cfb1019ee235045fa26c63b6141df4abdccb0
Clemens Schmid <clemens@nevrome.de>
2020-05-11 22:58:20 +0200

    made map plot more nice and powerful

--- R/helpers.R
+++ R/helpers.R
@@ -19,48 +19,80 @@ fuzzy_search <- function(pattern, search_vector) {
 #' @param lat test
 #'
 #' @export
-ascii_world_map <- function(lon, lat) {
+ascii_world_map <- function(lon = c(), lat = c()) {
   
-  points_of_interest_cells_plot <- tibble::tibble(
-      x = as.integer(cut(
-        lon, breaks = c(
-          sidora.cli::world_map_data$x_breaks$x_coord_start[1], 
-          sidora.cli::world_map_data$x_breaks$x_coord_stop
-        ), 
-        labels = 1:sidora.cli::world_map_data$plot_width
-      )),
-      y = as.integer(cut(
-        lat, breaks = c(
-          sidora.cli::world_map_data$y_breaks$y_coord_start[1],
-          sidora.cli::world_map_data$y_breaks$y_coord_stop
-        ), 
-        labels = 1:sidora.cli::world_map_data$plot_height
-      )),
-      position = TRUE
-    )
+  if (length(lon) == 0 | length(lat) == 0) {
+    
+    plot_grid <- sidora.cli::world_map_data$plot_grid %>%
+      dplyr::mutate(
+        position = FALSE
+      )
+    
+  } else {
   
-  # compiling plot grid
-  plot_grid <- sidora.cli::world_map_data$plot_grid %>%
-    dplyr::left_join(
-      points_of_interest_cells_plot, by = c("x", "y")
-    ) %>%
-    dplyr::mutate(
-      position =  tidyr::replace_na(.data[["position"]], FALSE)
-    )
+    points_of_interest_cells_plot <- tibble::tibble(
+        x = as.integer(cut(
+          lon, breaks = c(
+            sidora.cli::world_map_data$x_breaks$x_coord_start[1], 
+            sidora.cli::world_map_data$x_breaks$x_coord_stop
+          ), 
+          labels = 1:sidora.cli::world_map_data$plot_width
+        )),
+        y = as.integer(cut(
+          lat, breaks = c(
+            sidora.cli::world_map_data$y_breaks$y_coord_start[1],
+            sidora.cli::world_map_data$y_breaks$y_coord_stop
+          ), 
+          labels = 1:sidora.cli::world_map_data$plot_height
+        )),
+        position = TRUE
+      ) %>%
+      dplyr::group_by(.data[["x"]], .data[["y"]]) %>%
+      dplyr::summarise(
+        number_of_samples = sum(position),
+        position = any(position)
+      ) %>%
+      dplyr::ungroup()
+    
+    # compiling plot grid
+    plot_grid <- sidora.cli::world_map_data$plot_grid %>%
+      dplyr::left_join(
+        points_of_interest_cells_plot, by = c("x", "y")
+      ) %>%
+      dplyr::mutate(
+        number_of_samples =  tidyr::replace_na(.data[["number_of_samples"]], FALSE),
+        position =  tidyr::replace_na(.data[["position"]], FALSE)
+      )
+  
+  }
   
   # ascii plot
+  cat("\u25AE")
+  cat(rep("\u2014", sidora.cli::world_map_data$plot_width + 2), sep = "")
+  cat("\u25AE")
+  cat("\n")
   for (ro in seq(sidora.cli::world_map_data$plot_height, 1, -1)) {
+    cat("| ")
+    row_number_of_samples <- 0
     for (co in 1:sidora.cli::world_map_data$plot_width) {
       cur <- plot_grid[plot_grid$x == co & plot_grid$y == ro,]
       if (!cur$land & !cur$position) {
         cat(" ")
       } else if (cur$land & !cur$position) {
-        cat(".")
+        cat("\u25CB")
       } else if (cur$position) {
-        cat("X")
+        cat("\u25CF")
+        row_number_of_samples <- row_number_of_samples + cur$number_of_samples
       }
+    }
+    cat(" |")
+    if (row_number_of_samples > 0) {
+      cat(" \u25C0", row_number_of_samples)
     }
     cat("\n")
   }
+  cat("\u25AE")
+  cat(rep("\u2014", sidora.cli::world_map_data$plot_width + 2), sep = "")
+  cat("\u25AE")
   
 }
