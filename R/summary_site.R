759bb0eff17b86db110bfc8c513085f7993b0564
Clemens Schmid <clemens@nevrome.de>
2020-05-15 14:08:51 +0200

    more nice summary function for site

--- R/summary_site.R
+++ R/summary_site.R
@@ -7,17 +7,41 @@
 #' @export
 summary_site <- function(con, entity_id, cache_dir) {
 
+  # get data
   sites <- sidora.core::get_df(con, tab = "TAB_Site", cache_dir = cache_dir)
+  individuals <- sidora.core::get_df(con, tab = "TAB_Individual", cache_dir = cache_dir)
   
-  selected <- sites %>% dplyr::filter(.data[["Site_Id"]] == entity_id[1] | .data[["Name"]] == entity_id[1])
+  # filter and merge
+  sel_basic <- sites %>% dplyr::filter(.data[["Site_Id"]] == entity_id[1] | .data[["Name"]] == entity_id[1])
+  sel_individuals <- dplyr::left_join(
+    sel_basic %>% dplyr::rename("Site" = "Id"), 
+    individuals,
+    by = "Site",
+    suffix = c(".Site", ".Individual")
+  )
   
-  selected[c("Site_Id", "Name", "Country", "Tags", "Projects")] %>% knitr::kable() %>% print()
-  
-  if (nrow(selected) > 0) {
-    sidora.cli::ascii_world_map(selected$Longitude, selected$Latitude)
-  }
-  
-  if (nrow(selected) == 0) {
+  if (nrow(sel_basic) > 0) {
+    
+    # Site name
+    cat(
+      crayon::bold("Site: ") %+% 
+      crayon::green(sel_basic$Name, paste0("(", sel_basic$Site_Id ,")")) %+% 
+      "\n"
+    )
+    cat("---\n")
+    
+    # Individuals from this site
+    cat(
+      crayon::underline("Individuals:") %+% " " %+% paste0(
+        sel_individuals$Full_Individual_Id, 
+        collapse = ", "
+      ) %+% 
+      "\n"
+    )
+      
+    sidora.cli::ascii_world_map(sel_basic$Longitude, sel_basic$Latitude)
+    
+  } else {
     sidora.cli::fuzzy_search(entity_id[1], c(sites$Site_Id, sites$Name))
   }
   
