5be4cf8e939861c8f45aa814db3c10776853b14f
Clemens Schmid <clemens@nevrome.de>
2020-05-16 14:45:05 +0200

    added library summary

---
+++ R/summarise_library.R
@@ -0,0 +1,59 @@
+#' summarise_library
+#'
+#' @param con test
+#' @param entity_id test
+#' @param cache_dir test
+#'
+#' @export
+summarise_library <- function(
+  con = sidora.core::get_pandora_connection(), 
+  entity_id = "FUT001.A0101", 
+  cache_dir = "/tmp/sidora.cli_table_cache"
+) {
+  
+  # get data
+  libraries <- sidora.core::get_df(con, tab = "TAB_Library", cache_dir = cache_dir)
+  # filter
+  sel_basic <- libraries %>% dplyr::filter(.data[["Full_Library_Id"]] == entity_id[1])
+  
+  
+  if (nrow(sel_basic) > 0) {
+    
+    # get additional data and merge
+    sel_merged <- sidora.core::join_pandora_tables(x = list(
+      "TAB_Extract" = sidora.core::get_df(tab = "TAB_Extract", con = con, cache_dir = cache_dir) %>% 
+        dplyr::filter(Id == sel_basic$Extract),
+      "TAB_Library" = sel_basic,
+      "TAB_Capture" = sidora.core::get_df(con, tab = "TAB_Capture", cache_dir = cache_dir),
+      "TAB_Sequencing" = sidora.core::get_df(con, tab = "TAB_Sequencing", cache_dir = cache_dir)
+    ))
+    
+    
+    # Title
+    cat("---\n")
+    cat(
+      crayon::underline("Library:") %+% " " %+% 
+        crayon::silver(crayon::bold(
+          sel_basic$Full_Library_Id
+        ))  %+% " " %+%
+        crayon::blue(
+          "from extract", unique(sel_merged$Full_Extract_Id)
+        ) %+% 
+        "\n"
+    )
+    cat("---\n")
+    
+    # Captures and sequencing runs from this library
+    cat(
+      crayon::underline("Sequencing runs (SG -> no capture):") %+% " " %+% paste0(
+        crayon::bgGreen(sel_merged$Full_Sequencing_Id),
+        collapse = ", "
+      ) %+% 
+        "\n"
+    )
+    
+  } else {
+    sidora.cli::fuzzy_search(entity_id[1], libraries$Full_Library_Id)
+  }
+  
+}
