0b52716980dabb36294840f3b40878fbfada3aff
Clemens Schmid <clemens@nevrome.de>
2020-05-17 11:11:20 +0200

    added some comments

--- R/tabulate_module.R
+++ R/tabulate_module.R
@@ -16,31 +16,37 @@ tabulate_module <- function(
   cache_dir = "/tmp/sidora.cli_table_cache"
 ) {
 
+  # transform table names
   entity_type_table <- sidora.cli::convert_option_to_pandora_table(entity_type)$pandora_table
   filter_entity_type_table <- sidora.cli::convert_option_to_pandora_table(filter_entity_type)$pandora_table
   
-  # TODO: Function to fill the table sequence
+  # get the relevant tables and the tables in between
   table_list <- sidora.core::get_df_list(
+    # TODO: Function to fill the table sequence automatically
     c(entity_type_table, "TAB_Individual", filter_entity_type_table), 
     con = con, cache_dir = cache_dir
   )
   
+  # apply filter operation on filter entity
   table_list[[filter_entity_type_table]] <- eval(parse(text = 
    "table_list[[filter_entity_type_table]] %>% dplyr::filter(" %+%
    filter_string %+%
    ")"
   ))
-
+  
+  # create big, merged table
+  # TODO: Better way to filter to final result, maybe another join function that starts from the filter entity
   joined_table <- sidora.core::join_pandora_tables(table_list) 
 
-  # TODO: Better way to filter to final result, maybe another join function
   firstup <- function(x) {
     substr(x, 1, 1) <- toupper(substr(x, 1, 1))
     x
   }
   
   result_table <- joined_table %>%
+    # filter merged table to only include values in filtered filter entity
     dplyr::filter(!is.na(!!rlang::sym(paste0(firstup(filter_entity_type), "_Id")))) %>%
+    # reduce result variable selection to requested entity
     dplyr::select(colnames(table_list[[entity_type_table]]))
   
   print(result_table)
