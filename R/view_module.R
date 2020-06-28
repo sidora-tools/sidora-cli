4fd749fbf18f9877bdf5493898801eb274459363
Clemens Schmid <clemens@nevrome.de>
2020-06-28 19:14:05 +0200

    brought view module back to life

--- R/view_module.R
+++ R/view_module.R
@@ -10,37 +10,44 @@
 #' @export
 view_module <- function(con, entity_type, entity_id, cache_dir) {
 
-  ## Get corresponding table from our 'clean' sidora.cliinput options
-  tab_info <- sidora.cli::convert_option_to_pandora_table(entity_type)
+  selected_table <- sidora.core::get_df(
+    tab = sidora.core::entity_type_to_table_name(entity_type),
+    con = con, 
+    cache_dir = cache_dir
+  ) 
   
-  selected_table <- sidora.core::get_df(con, 
-                                        tab = tab_info$pandora_table, 
-                                        cache_dir = cache_dir)
+  filtered_table <- selected_table %>% dplyr::filter(
+    !!dplyr::sym(sidora.core::namecol_for_entity_type(entity_type)) == entity_id
+  )
 
-  table_filtered <- selected_table %>% 
-    dplyr::filter(eval(as.symbol(tab_info$id_column)) == entity_id, 
-                  !eval(as.symbol(paste0(entity_type, ".Deleted"))))
-  
-  ## Check table isn't empty, and do fuzzy search if true
-  if (nrow(table_filtered) == 0) {
-    search_vec <- selected_table %>% dplyr::pull(eval(as.symbol(tab_info$id_column)))
+  ## Check table is empty, and do fuzzy search if true
+  if (nrow(filtered_table) == 0) {
+    search_vec <- selected_table[[sidora.core::namecol_for_entity_type(entity_type)]]
     sidora.cli::fuzzy_search(entity_id, search_vec)
   } else {
     ## Find columns to get human-readable strings for get_name_from_id
-    cols2update <- names(table_filtered[sidora.core::col_name_has_aux(entity_type = entity_type, col_name = names(table_filtered))])
+    cols2update <- names(
+      filtered_table[, sidora.core::col_name_has_aux(entity_type = entity_type, col_name = names(filtered_table))]
+    )
     
     ## Format for printing, not tidy output, so suppressed warnings from gather
     suppressWarnings(
-      table_filtered %>%
-        dplyr::select(-paste0(entity_type, ".Id"), 
-                      -paste0(entity_type, ".Deleted"), 
-                      -paste0(entity_type, ".Deleted_Date")) %>%
-        dplyr::mutate(dplyr::across(all_of(cols2update), 
-                                    ~sidora.core::get_name_from_id(con = con, 
-                                                      query_tab = entity_type, 
-                                                      query_col = dplyr::cur_column(), 
-                                                      query_id = .x, 
-                                                      cache_dir = cache_dir))) %>%
+      filtered_table %>%
+        dplyr::select(
+          -paste0(entity_type, ".Id"), 
+          -paste0(entity_type, ".Deleted"), 
+          -paste0(entity_type, ".Deleted_Date")
+        ) %>%
+        dplyr::mutate(dplyr::across(
+          tidyselect::all_of(cols2update), 
+          ~sidora.core::namecol_value_from_id(
+            con = con, 
+            query_tab = entity_type, 
+            query_col = dplyr::cur_column(), 
+            query_id = .x, 
+            cache_dir = cache_dir
+          )
+        )) %>%
         tidyr::gather("Field", "Value", 1:ncol(.)) %>% 
         dplyr::mutate(Field = gsub(paste0(entity_type, "\\."), "", .data$Field)) %>%
         knitr::kable() %>% 
