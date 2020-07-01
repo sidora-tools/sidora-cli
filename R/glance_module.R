6436c8c5f287564cc9217177343f4105b5cae753
Clemens Schmid <clemens@nevrome.de>
2020-07-01 22:15:54 +0200

    made glance more similar to view

--- R/glance_module.R
+++ R/glance_module.R
@@ -13,20 +13,32 @@ glance_module <- function(
   cache_dir = "~/.sidora"
 ) {
   
-  entity_type_table <- sidora.core::entity_type_to_table_name(entity_type)
-  
-  result_table <- sidora.core::get_df(
-    entity_type_table,
+  # get relevant table
+  sidora.core::get_df(
+    sidora.core::entity_type_to_table_name(entity_type),
     con = con, cache_dir = cache_dir
-  )
+  ) %>%
+    # count number of "empty" columns per row
+    dplyr::mutate(
+      number_of_na = rowSums(is.na(.) | . == "")
+    ) %>%
+    # arrange by that count
+    dplyr::arrange(.data[["number_of_na"]]) %>%
+    dplyr::select(-.data[["number_of_na"]]) %>%
+    # only keep rows with no/low number of missing values
+    magrittr::extract(1:3, ) %>%
+    # transpose table and transform to two column table
+    t() %>%
+    magrittr::set_colnames(c("V1", "V2", "V3")) %>%
+    tibble::as_tibble(rownames = "Field") %>%
+    tidyr::unite("Examples", tidyselect::starts_with("V"), sep = "; ") %>%
+    dplyr::mutate(
+      # remove weird linebreaks
+      Examples = gsub("\n", "/", .data[["Examples"]]),
+      # cut string length
+      Examples = ifelse(nchar(Examples) > 43, paste0(strtrim(Examples, 40), '...'), Examples)
+    ) %>%
+    knitr::kable(col.names = c("Fields", "3 example values"))
   
-  for (i in 1:ncol(result_table)) {
-    
-    cat(crayon::bold(colnames(result_table)[i]), ": ", sep = "")
-    first_three_values <- result_table[1:3,] %>% dplyr::pull(i)
-    cat(first_three_values, sep = ", ")
-    cat(", ...\n")
-    
-  }
   
 }
